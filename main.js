3c02fc9abfcd3d57ced961a1b1efcc1b6a708a00
Esteban Bentancor <ebentancor341@gmail.com>
2023-09-05 21:03:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ const boton = document.getElementById('boton');
 boton.addEventListener('click',function(){
 form.submit()
 });
+
+fetch("https://jsonplaceholder.typicode.com/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(datos)
+  })
+  .then(response => response.text())
+  .then(data => {
+    console.log(data); // Puedes hacer algo con la respuesta del servidor
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
